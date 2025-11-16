3a35ed6752933fe546db0a826c645dabab78a572
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2025-11-16 17:57:15 +0330

    Add project files.

---
+++ src/03. EndPoint/Car-Inspection.EndPoint.Razor/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Car_Inspection.EndPoint.Razor.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
