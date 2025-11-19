1e65364323bdf7959b4da284c418da46424f6616
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2025-11-19 22:00:35 +0330

    fix some error

---
+++ src/02. Infrastructure/Db/Car-Inspection.Infa.Db.SqlServer.EfCore/Migrations/20251119181405_update-appointment.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Car_Inspection.Infa.Db.SqlServer.EfCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateappointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerFirstName",
+                table: "Appointments",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerLastName",
+                table: "Appointments",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "OwnerFirstName", "OwnerLastName" },
+                values: new object[] { "محمد", "حسینی" });
+
+            migrationBuilder.UpdateData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "OwnerFirstName", "OwnerLastName" },
+                values: new object[] { "فاطمه", "شفیعی" });
+
+            migrationBuilder.UpdateData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "OwnerFirstName", "OwnerLastName" },
+                values: new object[] { "لیلا", "نرده ای" });
+
+            migrationBuilder.UpdateData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "OwnerFirstName", "OwnerLastName" },
+                values: new object[] { "علی", "رضایی" });
+
+            migrationBuilder.UpdateData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "OwnerFirstName", "OwnerLastName" },
+                values: new object[] { "سنا", "محمدی" });
+
+            migrationBuilder.UpdateData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "OwnerFirstName", "OwnerLastName" },
+                values: new object[] { "نیما", "شاد" });
+
+            migrationBuilder.UpdateData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "OwnerFirstName", "OwnerLastName" },
+                values: new object[] { "مینا", "رسولی" });
+
+            migrationBuilder.UpdateData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "OwnerFirstName", "OwnerLastName" },
+                values: new object[] { "حسین", "اکبری" });
+
+            migrationBuilder.UpdateData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "OwnerFirstName", "OwnerLastName" },
+                values: new object[] { "زهرا", "شالی" });
+
+            migrationBuilder.UpdateData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "OwnerFirstName", "OwnerLastName" },
+                values: new object[] { "رامین", "کاظمی" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerFirstName",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerLastName",
+                table: "Appointments");
+        }
+    }
+}
--- src/03. EndPoint/Car-Inspection.EndPoint.Razor/Pages/Index.cshtml.cs
+++ src/03. EndPoint/Car-Inspection.EndPoint.Razor/Pages/Index.cshtml.cs
@@ -1,12 +1,42 @@
+using Car_Inspection.Domain.Core.Appointment.AppServices;
+using Car_Inspection.Domain.Core.Appointment.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Car_Inspection.EndPoint.Razor.Pages
 {
-    public class IndexModel : PageModel
+    public class AppointmentManagerModel(IAppointmentAppService appointmentAppService) : PageModel
     {
+
+
+
+        [BindProperty]
+        public CreateAppointmentDto Appointment { get; set; }
+
+
+        public List<AppointmentDto> Appointments { get; set; }
+
         public void OnGet()
         {
+
+            Appointments = appointmentAppService.GetAll();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var result = appointmentAppService.Create(Appointment);
+
+            if (!result.IsSuccess)
+            {
+                ModelState.AddModelError(string.Empty, result.Message);
+                return Page();
+            }
+
+            TempData["Success"] = result.Message;
+            return RedirectToPage(); 
         }
     }
 }
